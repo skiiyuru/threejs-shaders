dd2f1e04ab68f1442c0c12eec795b2b6c6cda8b8
StephenKiiyuruNganga <steveknganga@gmail.com>
2023-10-26 13:23:25 +0300

    add custom shader

---
+++ src/shaders/test/vertex.glsl
@@ -0,0 +1,35 @@
+// uniform mat4 projectionMatrix;
+// uniform mat4 viewMatrix;
+// uniform mat4 modelMatrix;
+uniform vec2 uFrequency;
+uniform float uTime;
+
+// attribute vec3 position;
+attribute float aSpike;
+// attribute vec2 uv;
+
+// varying float vSpike;
+varying vec2 vUv;
+varying float vElevation;
+
+void main() {
+	vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+	float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+	elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+	modelPosition.z += elevation;
+
+	// modelPosition.z += aSpike * 0.1;
+
+	vec4 viewPosition = viewMatrix * modelPosition;
+	vec4 projectedPosition = projectionMatrix * viewPosition;
+
+	gl_Position = projectedPosition;
+
+	/* update varyings */
+
+	// vSpike = aSpike;
+	vUv = uv;
+	vElevation = elevation;
+}
